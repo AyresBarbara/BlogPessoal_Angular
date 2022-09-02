343733eeb8500635dbf2a5ca42e7298c4234d6ba
Barbara Ayres <ayresbarbara2@gmail.com>
2022-09-02 15:57:46 -0300

    Componente Entrar

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,13 +5,15 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { MenuComponent } from './menu/menu.component';
 import { FooterComponent } from './footer/footer.component';
+import { EntrarComponent } from './entrar/entrar.component';
 
 
 @NgModule({
   declarations: [
     AppComponent,
     MenuComponent,
-    FooterComponent
+    FooterComponent,
+    EntrarComponent
   ],
   imports: [
     BrowserModule,
